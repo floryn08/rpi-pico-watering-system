fd2e57d4aa7875974e41347dbc352bc83d00bd4b
Florin Vasilica <floryn.euro.2007@gmail.com>
2022-07-28 21:51:54 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# TODO
+from machine import ADC, Pin
+from utime import sleep 
+
+pot = ADC(26)
+
+maxValue=65535
+minValue=26000
+
+def convert_to_percent(value):
+    
+    percent = 100 - (value-minValue)/(maxValue-minValue) * 100
+    return percent
+    
+
+while True:
+     raw = pot.read_u16()
+     print('Raw: {} '.format(raw))
+     print(str(convert_to_percent(raw)) + "%")
+     sleep(1)
