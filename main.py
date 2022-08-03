b662d76b82e281b6ca91c3dba8b8b2005dfc6021
Flow <floryn.euro.2007@gmail.com>
2022-08-03 18:36:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 from machine import ADC, Pin
-from utime import sleep 
+from utime import sleep
+import dht
 
-pot = ADC(26)
+sensor1 = Pin(22, Pin.IN)
+sensor2 = Pin(26, Pin.IN)
+sensor3 = Pin(27, Pin.IN)
 
-maxValue=65535
-minValue=26000
+relay1 = Pin(21, Pin.OUT, value=1)
+relay2 = Pin(20, Pin.OUT, value=1)
+relay3 = Pin(19, Pin.OUT, value=1)
+relay4 = Pin(18, Pin.OUT, value=1)
 
-def convert_to_percent(value):
-    
-    percent = 100 - (value-minValue)/(maxValue-minValue) * 100
-    return percent
-    
+tempSensor = dht.DHT11(Pin(15, Pin.IN))
 
 while True:
-     raw = pot.read_u16()
-     print('Raw: {} '.format(raw))
-     print(str(convert_to_percent(raw)) + "%")
-     sleep(1)
+    
+     #try:
+         #sleep(2)
+         #tempSensor.measure()
+         #temp = tempSensor.temperature()
+         #hum = tempSensor.humidity()
+         #print(temp)
+         #print(hum)
+     #except OSError as e:
+         #print('Failed to read sensor.')
+     
+     if(sensor1.value() == 1):
+         relay1.on()
+         relay4.on()
+     else:
+         relay1.off()
+         relay4.off()
+         
+     if(sensor2.value() == 1):
+         relay1.on()
+         relay3.on()
+     else:
+         relay1.off()
+         relay3.off()
+         
+     if(sensor3.value() == 1):
+         relay1.on()
+         relay2.on()
+     else:
+         relay1.off()
+         relay2.off()
